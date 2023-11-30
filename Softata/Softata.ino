7a16845ff846535e256ad391a1ddfa1629e481e4
djaus2 <davidjones@sportronics.com.au>
2023-11-30 22:33:01 +1100

    Add project files.

---
+++ Softata/Softata.ino
@@ -0,0 +1,254 @@
+// Placed in the public domain by Earle F. Philhower, III, 2022
+#include "rpiboards.h"
+#include <WiFi.h>
+
+#ifndef STASSID
+#define STASSID "APQLZM"
+#define STAPSK "silly1371"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+int port = 4242;
+
+WiFiServer server(port);
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.setHostname("PicoW2");
+  Serial.printf("Connecting to '%s' with '%s'\n", ssid, password);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.printf("\nConnected to WiFi\n\nConnect to server at %s:%d\n", WiFi.localIP().toString().c_str(), port);
+
+  server.begin();
+}
+
+void loop() {
+  static int i;
+  delay(100);
+  //Serial.printf("--loop %d\n", ++i);
+  //delay(10);
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  Serial.println("WiFi Up");
+  while(true)
+  {
+    Serial.print("Get next command.");
+    while (!client.available()) {
+      delay(100);
+    }
+
+    Serial.println("Connected.");
+    while (client.available()) {
+      String req = client.readStringUntil('\n');
+      if(req.length()==0)
+      {
+        Serial.println("Null cmd.");
+        return;
+      }
+      Serial.print("Command:");
+      Serial.println(req[0],HEX);
+      switch(req[0])
+      {
+        // Escape simple string commands here. 
+        // These commands sto start in uppercase
+        // Need first letter of these ASCII values to not match commands
+        // ie Softata commands not to be between 65 to 90, 0x41 to 0x5A
+        case 'B':  // Begin
+          Serial.println("Ready.");
+          client.print("Ready"); // Sent at first connection.
+          break;
+        case 'E': //End
+          Serial.println("Done.");
+          client.print("Done"); 
+          return;
+          break;
+        default:
+          // Get Softata command and parameters
+          byte cmd = req[0];
+          byte pin = 0xff;
+          byte param = 0xff;
+          byte other = 0xff;
+          if(req.length()>1)
+          {
+            pin=req[1];
+            if(req.length()>2)
+            {
+              param=req[2];
+              if(req.length()>3)
+              {
+                other=req[3];
+          } } }
+
+          //Print commd and paramaters
+          String str;
+          byte vaue;
+          Serial.print("cmd:");
+          Serial.print(cmd);Serial.print(' ');
+          if (pin!=0xff)
+          {
+            Serial.print("pin:");
+            Serial.print(pin,HEX);Serial.print(' ');
+            if (param!=0xff)
+            {
+              Serial.print("param:");
+              Serial.print(param);Serial.print(' ');
+              if (other!= 0xff)
+              {
+                Serial.print("other:");
+                Serial.print(other);
+          } } }
+          Serial.println();
+
+          // Action cmds
+          int value;
+          switch(cmd)
+          {
+            case 0xD0:
+            case 0xD1:
+            case 0xD2:
+            case 0xD3:
+            { 
+              // Digital
+              if(!IS_PIN_DIGITAL(pin))
+              {
+                Serial.print("Pin is not digital");
+                client.print("NOK");
+                return;
+              }
+              switch (cmd)
+              {
+                case 0xD0:
+                  Serial.print("pinMode:");
+                  Serial.println(param);
+                  pinMode(pin, (PinMode)param);
+                  client.print("OK");
+                  break;
+                case 0xD1:
+                  if(!IS_PIN_DIGITAL(pin))
+                  Serial.println("digitalRead");
+                  value = digitalRead(pin);
+                  if(value)
+                    client.print("ON");
+                  else
+                    client.print("OFF");
+                  break;
+                case 0xD2:
+                  Serial.print("digitalWrite:");
+                  Serial.println(param);
+                  if(param)
+                    digitalWrite(pin, HIGH); 
+                  else
+                    digitalWrite(pin, LOW);
+                  client.print("OK");
+                  break; 
+                case 0xD3:
+                  Serial.print("digitalToggle:");
+                  value = digitalRead(pin);
+                  if(value)
+                    digitalWrite(pin, LOW); 
+                  else
+                    digitalWrite(pin, HIGH);
+                  client.print("OK");
+                  break;                   
+                default:
+                  Serial.println("Unknown digital cmd");
+                  client.print("Unknown digital cmd"); 
+                  break;              
+              }
+              break;           
+            }
+            case 0xA0:
+            case 0xA1:
+            case 0xA2: // Analog place holder
+              if(!IS_PIN_ANALOG(pin))
+              {
+                Serial.print("Pin not Analog");
+                client.print("NOK");
+                return;
+              }
+              Serial.println("Analog 2D cmds");
+              client.print("Analog 2D cmds"); 
+              break;
+            case 0xB0:
+            case 0xB1:
+            case 0xB2: // PWM place holder
+              if(!IS_PIN_PWM(pin))
+              {
+                Serial.print("Pin not PWM");
+                client.print("NOK");
+                return;
+              }
+              Serial.println("PWD 2D cmds");
+              client.print("PWD 2D cmds"); 
+              break;
+            case 0xC0:
+            case 0xC1:
+            case 0xC2: // Servo place holder
+              if(!IS_PIN_SERVO(pin))
+              {
+                Serial.print("Pin not Servo");
+                client.print("NOK");
+                return;
+              }
+              Serial.println("SERVO 2D cmds");
+              client.print("SERVO 2D cmds"); 
+              break;
+            case 0xE0:
+            case 0xE1:
+            case 0xE2: // Serial place holder
+              if(!IS_PIN_SERIAL(pin))
+              {
+                Serial.print("Pin not Serial");
+                client.print("NOK");
+                return;
+              }
+              Serial.println("Serial 2D cmds");
+              client.print("Serial 2D cmds"); 
+              break;
+            case 0xF0:
+            case 0xF1:
+            case 0xF2: // I2C place holder
+              if(!IS_PIN_I2C(pin))
+              {
+                Serial.print("Pin not I2C");
+                client.print("NOK");
+                return;
+              }
+              Serial.println("I2C 2D cmds");
+              client.print("I2C 2D cmds"); 
+              break;/*
+            case 0xF3:
+            case 0xF4:
+            case 0xF5: // SPI place holder
+              if(!IS_PIN_SPI(pin)))
+              {
+                Serial.print("Pin not SPI");
+                client.print("NOK");
+                return;
+              }
+              Serial.println("SPI 2D cmds");
+              client.print("SPI 2D cmds"); 
+              break;*/
+            default:
+              Serial.println("Unknown cmd");
+              client.print("Unknown cmd"); 
+              break; 
+          }
+          break;            
+      }
+      delay(500);
+    }
+  }
+
+  client.printf("Done from Pico-W\r\n");
+  client.flush();
+}
---
+++ SoftataConsole/Program.cs
@@ -0,0 +1,337 @@
+﻿using Microsoft.VisualBasic;
+using System.Collections.Concurrent;
+using System.Data.Common;
+
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace FirmataBasic
+{
+    internal class Program
+    {
+        const int port = 4242;//27016;
+        const string ipaddressStr = "192.168.0.13";
+
+        private static Socket client;
+
+        public enum Commands 
+        {
+            pinMode=0xD0,
+            digitalWrite= 0xD1,
+            digitalRead= 0xD2,
+            digitalToggle=0xD3,
+
+            Undefined = 0xFF
+        }
+
+        public enum CommandType :byte
+        {
+            Digital = 0,
+            Analog = 1,
+            PWM = 2,
+            Servo = 3,
+            I2C=4,
+            SPI=5,
+            Undefined = 0xFF,
+        }   
+        public enum PinMode
+        {
+            Undefined = -1,
+            DigitalInput = 0,
+            DigitalOutput = 1,
+            AnalogInput = 2,
+            PwmOutput = 3,
+            ServoControl = 4,
+            I2C = 6,
+            OneWire = 7,
+            StepperControl = 8,
+            Encoder = 9,
+            Serial = 10,
+            InputPullup = 11
+        }
+
+        public enum  PinState
+        {
+            LOW = 0x00,
+            HIGH = 0x01
+        }
+        private enum MessageHeader
+        {
+            AnalogState = 0xE0, // 224
+            DigitalState = 0x90, // 144
+            SystemExtension = 0xF0,
+            ProtocolVersion = 0xF9
+        }
+
+        private enum StringReadMode
+        {
+            ReadLine,
+            ReadToTerminator,
+            ReadBlock
+        }
+
+        private class StringRequest
+        {
+            private readonly StringReadMode _mode;
+            private readonly int _blockLength;
+            private readonly char _terminator;
+
+            public static StringRequest CreateReadLineRequest()
+            {
+                return new StringRequest(StringReadMode.ReadLine, '\\', 0);
+            }
+
+            public static StringRequest CreateReadRequest(int blockLength)
+            {
+                return new StringRequest(StringReadMode.ReadBlock, '\\', blockLength);
+            }
+
+            public static StringRequest CreateReadRequest(char terminator)
+            {
+                return new StringRequest(StringReadMode.ReadToTerminator, terminator, 0);
+            }
+
+            private StringRequest(StringReadMode mode, char terminator, int blockLength)
+            {
+                _mode = mode;
+                _blockLength = blockLength;
+                _terminator = terminator;
+            }
+
+            public char Terminator => _terminator;
+            public int BlockLength => _blockLength;
+            public StringReadMode Mode => _mode;
+        }
+
+        private const byte Pin_Mode = 0xF4;
+        private const byte AnalogMessage = 0xE0;
+        private const byte DigitalMessage = 0x90;
+        private const byte SysExStart = 0xF0;
+        private const byte SysExEnd = 0xF7;
+
+        private const int Buffersize = 2048;
+        private const int MaxQueuelength = 100;
+
+        //private readonly ISerialConnection client;
+        //private readonly bool _gotOpenConnection;
+        //private readonly LinkedList<FirmataMessage> _receivedMessageList = new LinkedList<FirmataMessage>();
+        //private readonly Queue<string> _receivedStringQueue = new Queue<string>();
+        //private ConcurrentQueue<FirmataMessage> _awaitedMessagesQueue = new ConcurrentQueue<FirmataMessage>();
+        //private ConcurrentQueue<StringRequest> _awaitedStringsQueue = new ConcurrentQueue<StringRequest>();
+        //private StringRequest _currentStringRequest;
+
+        //private int _messageTimeout = -1;
+        //private ProcessMessageHandler _processMessage;
+        private int _messageBufferIndex, _stringBufferIndex;
+        private readonly byte[] _messageBuffer = new byte[Buffersize];
+        private readonly char[] _stringBuffer = new char[Buffersize];
+
+        static void SendMessagCmd(string cmd)
+        {
+            string sendmsg = cmd;
+            Console.WriteLine("Sending: " + sendmsg);
+            sendmsg += "\n";
+            byte[] msg = Encoding.ASCII.GetBytes(sendmsg);
+            int sent = client.Send(msg); ;
+
+            Console.WriteLine($"Sent {sent-1} bytes");
+
+            while (client.Available == 0) ;
+            byte[] data = new byte[100];
+            int recvd = client.Receive(data);
+
+            string res = Encoding.ASCII.GetString(data);
+            Console.WriteLine($"Received {res} [{recvd}] bytes\n");
+
+            switch (cmd)
+            {
+                case "Begin":
+                    break;
+                case "End":
+                    client.Shutdown(SocketShutdown.Both);
+                    client.Close();
+                    break;  
+            }
+
+        }
+        static string SendMessage(Commands MsgType, byte pin=0xff, byte state=0xff, byte other=0xff)
+        {
+            string result = "";
+            // Construct command and parameters as list of bytes
+            List<byte> sendmsg = new List<byte> {(byte)MsgType};
+            if (pin != 0xff)
+            {
+                sendmsg.Add(pin);
+                if (state != 0xff)
+                {
+                    sendmsg.Add(state);
+                    if (other != 0xff)
+                    {
+                        sendmsg.Add(other);
+                    }
+                }
+            }
+
+            // Get bytes as string for display here.
+            byte[] sendBytes = sendmsg.ToArray<byte>();
+            string sendmsgStr = BitConverter.ToString(sendBytes).Replace(",", string.Empty);
+            Console.WriteLine("Sending: " +sendmsgStr);
+
+            // Add \n to end of message and send.
+            // The command terminator at other end
+            sendmsg.Add((byte)'\n');
+            sendBytes = sendmsg.ToArray<byte>();
+            int sent = client.Send(sendBytes);;
+            
+            Console.WriteLine($"Sent {sent-1} bytes");
+
+            //Wait for response
+            while (client.Available == 0);
+            byte[] data = new byte[100];
+            int recvd = client.Receive(data);
+
+            result = Encoding.ASCII.GetString(data);
+            Console.WriteLine($"Received {result} [{recvd}] bytes\n");
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            byte[] data = new byte[100];
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = (byte)0;
+            }   
+
+            IPHostEntry iphostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAdress = IPAddress.Parse(ipaddressStr); 
+            
+            IPEndPoint ipEndpoint = new IPEndPoint(ipAdress, port);
+
+            Console.WriteLine("Hello, World!");
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                client.Connect(ipEndpoint);
+                while(!client.Connected)
+                {
+                    Thread.Sleep(500);
+                }
+
+                Console.WriteLine("Socket created to {0}", client.RemoteEndPoint?.ToString());
+                byte[] sendmsg = new byte[] { Pin_Mode, (byte)32, (byte)PinMode.DigitalOutput};
+
+                SendMessagCmd("Begin");
+                Thread.Sleep(500);
+
+                Digital.SetPinMode(13, PinMode.DigitalInput);
+                Digital.SetPinMode(12, PinMode.DigitalOutput);
+                Digital.SetPinState(12, PinState.HIGH);
+
+                Digital.SetPinMode(50, PinMode.DigitalInput);
+
+                //SendMessage(0xF4, (byte)13, (byte)PinMode.DigitalInput);
+                Thread.Sleep(500);
+
+                int n;
+                for (int i = 0; i < 12; i++)
+                {;
+                    Digital.GetPinState(13);
+                    Digital.TogglePinState(12);
+                    //SendMessage(0x6D, (byte)13); ;
+                    //SendMessage(0x6E, (byte)13, (byte)PinState.HIGH);
+                    //Thread.Sleep(1000);
+                    //SendMessage(0x6E, (byte)13, (byte)PinState.LOW);
+                    Thread.Sleep(1000);
+                }
+
+                SendMessagCmd("End");
+                Thread.Sleep(500);
+                return;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                if(client.Connected)
+                {
+                    client.Shutdown(SocketShutdown.Both);
+                    client.Close();
+                }
+            }
+        }
+
+
+
+        public void SetDigitalPort(int portNumber, int pins)
+        {
+            if (portNumber < 0 || portNumber > 15)
+                throw new ArgumentOutOfRangeException(nameof(portNumber), "Messages.ArgumentEx_PortRange0_15");
+
+            if (pins < 0 || pins > 0xFF)
+                throw new ArgumentOutOfRangeException(nameof(pins), "Messages.ArgumentEx_ValueRange0_255");
+
+            client.Send(new[] { (byte)(DigitalMessage | portNumber), (byte)(pins & 0x7F), (byte)((pins >> 7) & 0x03), (byte)0 }); //, 0, 3);
+        }
+
+        /// <inheritdoc cref="IFirmataProtocol.SetDigitalReportMode"/>
+        public void SetDigitalReportMode(int portNumber, bool enable)
+        {
+            if (portNumber < 0 || portNumber > 15)
+                throw new ArgumentOutOfRangeException(nameof(portNumber), "Messages.ArgumentEx_PortRange0_15");
+
+            client.Send(new[] { (byte)(0xD0 | portNumber), (byte)(enable ? 1 : 0), (byte)0 }); //, 0, 2);
+        }
+
+        public static class Digital
+        {
+            public static void SetPinMode(int pinNumber, PinMode mode)
+            {
+                if (pinNumber < 0 || pinNumber > 127)
+                    throw new ArgumentOutOfRangeException(nameof(pinNumber), "Messages.ArgumentEx_PinRange0_127");
+
+                SendMessage(Commands.pinMode, (byte)pinNumber, (byte)mode);
+            }
+
+            public static void SetPinState(int pinNumber, PinState state)
+            {
+                if (pinNumber < 0 || pinNumber > 127)
+                    throw new ArgumentOutOfRangeException(nameof(pinNumber), "Messages.ArgumentEx_PinRange0_127");
+
+                SendMessage(Commands.digitalWrite, (byte)pinNumber, (byte)state);
+            }
+
+            public static void TogglePinState(int pinNumber)
+            {
+                if (pinNumber < 0 || pinNumber > 127)
+                    throw new ArgumentOutOfRangeException(nameof(pinNumber), "Messages.ArgumentEx_PinRange0_127");
+
+                SendMessage(Commands.digitalToggle, (byte)pinNumber);
+            }
+
+            public static bool GetPinState(int pinNumber)
+            {
+                if (pinNumber < 0 || pinNumber > 127)
+                    throw new ArgumentOutOfRangeException(nameof(pinNumber), "Messages.ArgumentEx_PinRange0_127");
+
+                string state = SendMessage(Commands.digitalRead, (byte)pinNumber);
+
+                if (state.ToUpper() == "ON")
+                    return true;
+                else
+                    return false;
+            }
+
+
+        }
+
+
+        public static class Analog
+        {
+            
+        }
+    }
+}
