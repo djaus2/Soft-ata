4351648bf77788a510f140a5c710841e44353b9c
djaus2 <davidjones@sportronics.com.au>
2023-12-03 22:25:56 +1100

    Tidied up tehe 2 Tests

--- SoftataConsole/Program.cs
+++ SoftataConsole/Program.cs
@@ -16,9 +16,21 @@ namespace FirmataBasic
 {
     internal class Program
     {
+        // Set the same as Arduino:
         const int port = 4242;
         const string ipaddressStr = "192.168.0.13";
- 
+
+        // Configure hardware pin connections thus:
+        static byte LED = 12;
+        static byte BUTTON = 13;
+        static byte POTENTIOMETER = 26;
+
+        // Choose test type
+        static SoftataLib.CommandType Testtype = CommandType.Digital;
+        // Next two are the same test
+        //static SoftataLib.CommandType Testtype = CommandType.Analog;
+        //static SoftataLib.CommandType Testtype = CommandType.PWM;
+
         static void Main(string[] args)
         {
             SoftataLib.Init(ipaddressStr, port);
@@ -27,57 +39,64 @@ namespace FirmataBasic
             try
             {
 
-                SoftataLib.SendMessageCmd("Begin");
-                
+                SoftataLib.SendMessageCmd("Begin");               
                 Thread.Sleep(500);
 
-                //SoftataLib.Digital.SetPinMode(13, SoftataLib.PinMode.DigitalInput);
-                SoftataLib.Digital.SetPinMode(12, SoftataLib.PinMode.DigitalOutput);
-                //SoftataLib.Digital.SetPinState(12, SoftataLib.PinState.HIGH);
-
-                // Next is errant as no pin 50 on Pico
-                //Digital.SetPinMode(50, PinMode.DigitalInput);
-
-                // Next is errant as no such command
-                //SoftataLib.SendMessage(SoftataLib.Commands.Undefined, (byte)26, (byte)PinMode.DigitalInput);
-
-
-                // Next is now errant
-                //SoftataLib.SendMessage(SoftataLib.Commands.analogWrite, (byte)26, (byte)PinMode.DigitalInput);
-                Thread.Sleep(500);
-
-                int n;
-                int ADPin = 26;
-                for (int i = 0; i < 50; i++)
+                switch (Testtype)
                 {
-                    //SoftataLib.Digital.GetPinState(13);
-                    //SoftataLib.Digital.TogglePinState(12);
-                    //SoftataLib.Digital.TogglePinState(12);
-                    int val  =SoftataLib.Analog.AnalogRead(ADPin);
-                    if (val != int.MaxValue)
-                    {
- 
-                        Console.WriteLine($"AnalogRead({ADPin}) = {val}");
-                        byte pwmVal = (byte)(val >>2);
-                        /*int pwmVal = (byte)(val / 4);*/
-                        if (val > 1023)
-                            pwmVal = 255;
-                        SoftataLib.PWM.SetPWM(12, (byte) (pwmVal));
-                    }
-                    else
-                        Console.WriteLine($"AnalogRead({ADPin}) failed");
-                    Console.WriteLine();
-                    Thread.Sleep(500);
-                }
+                    // LED-Button test
+                    case CommandType.Digital:
+                        SoftataLib.Digital.SetPinMode(BUTTON, SoftataLib.PinMode.DigitalInput);
+                        SoftataLib.Digital.SetPinMode(LED, SoftataLib.PinMode.DigitalOutput);
+                        SoftataLib.Digital.SetPinState(LED, SoftataLib.PinState.HIGH);
 
-                SoftataLib.SendMessageCmd("End");
-                Thread.Sleep(500);
-                return;
+                        // Next is errant as no pin 50 on Pico
+                        //Digital.SetPinMode(50, PinMode.DigitalInput);
+
+                        // Next is errant as no such command
+                        //SoftataLib.SendMessage(SoftataLib.Commands.Undefined, (byte)26, (byte)PinMode.DigitalInput);
+
+                        for (int i = 0; i < 10; i++)
+                        {
+                            while( SoftataLib.Digital.GetPinState(BUTTON))
+                                Thread.Sleep(500);
+                            SoftataLib.Digital.TogglePinState(LED);
+                        }
+                        break;
+
+                    // Potentiometer-LED Test
+                    case CommandType.Analog:
+                    case CommandType.PWM:
+                        SoftataLib.Digital.SetPinMode(LED, SoftataLib.PinMode.DigitalOutput);
+                        for (int i = 0; i < 50; i++)
+                        {
+                            int val = SoftataLib.Analog.AnalogRead(POTENTIOMETER);
+                            if (val != int.MaxValue)
+                            {
+                                Console.WriteLine($"AnalogRead({POTENTIOMETER}) = {val}");
+                                byte pwmVal = (byte)(val >> 2);
+                                if (val > 1023)
+                                    pwmVal = 255;
+                                SoftataLib.PWM.SetPWM(12, (byte)(pwmVal));
+                            }
+                            else
+                                Console.WriteLine($"AnalogRead({POTENTIOMETER}) failed");
+                            Console.WriteLine();
+                            Thread.Sleep(500);
+                        }
+                        break;
+                }
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
+
+            SoftataLib.SendMessageCmd("End");
+            Thread.Sleep(500);
+
+            return;
+
         }
     }
 }
