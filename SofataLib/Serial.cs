a671b5c029e3ccb09396e673a448d65d996781ba
djaus2 <davidjones@sportronics.com.au>
2024-01-25 23:28:04 +1100

    More fixes for  muckup!

--- SofataLib/Serial.cs
+++ SofataLib/Serial.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static NetStandard_Softata.SoftataLib;
+using static Softata.SoftataLib;
 
 namespace Softata
 {
