66ba5b6829e9cb3cd58a3a428a5ed2c73a1a9c3d
djaus2 <davidjones@sportronics.com.au>
2024-08-31 18:17:35 +1000

    Repost Console Relay code

--- code/SoftataConsole/Program.cs
+++ code/SoftataConsole/Program.cs
@@ -1091,7 +1091,7 @@ namespace SoftataBasic
                                                 break;
                                         }
                                     }
-                                }
+                                } 
                                 else
                                 {
                                     if (state)
